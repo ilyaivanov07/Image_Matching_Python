f2e6de14035154a8568f74d1dd75626f99ea0960
iivanov8 <iivanov8@gatech.edu>
2018-03-06 11:52:12 -0500

    saving

--- send_email.py
+++ send_email.py
@@ -2,27 +2,44 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-# server = smtplib.SMTP('doittsmtp.nycnet', 25)
-# msg = "YOUR MESSAGE!"
-# server.sendmail("iivanov@doitt.nyc.gov", "mikskuntz@gmail.com", msg)
-# server.quit()
-
-
+fromaddr = "mikskuntz@gmail.com"
+msg_subject = "OMSCS Survey"
+msg_body = "Hello\n\n. I am doing research for the OMSCS EdTech class. Please be kind to answer a couple of questions in this survey: \n\n https://docs.google.com/forms/d/e/1FAIpQLSf5YSiVYvwLdVMYMoqc8mcFEaMsq5g4cpq5wx6URsJsl0dWAQ/viewform?usp=sf_link"
 server = smtplib.SMTP('doittsmtp.nycnet', 25)
 server.starttls()
 
-for i in range(1):
-    # fromaddr = "iivanov@doitt.nyc.com"
-    fromaddr = "iivanov67@gmail.com"
-    toaddr = "iivanov67@yahoo.com"
+file_to_write = open("sent_emails","a")
+
+with open("addresses","r+") as f:
+    for line in f:
+        toaddr = line
+        msg = MIMEMultipart()
+        msg['From'] = fromaddr
+        msg['To'] = toaddr
+        msg['Subject'] = msg_subject
+        msg.attach(MIMEText(msg_body, 'plain'))
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
+
+        file_to_write.write(line)
+        print(line)
+
+file_to_write.write("\n")
+
+
+# for i in range(1):
+    #fromaddr = "iivanov@doitt.nyc.com"
+    #toaddr = "iivanov8@gatech.edu"
     #toaddr = "mikskuntz@gmail.com"
-    msg = MIMEMultipart()
-    msg['From'] = fromaddr
-    msg['To'] = toaddr
-    msg['Subject'] = "Survey"
-    body = "Take the survey:\n\n https://docs.google.com/forms/d/e/1FAIpQLSf5YSiVYvwLdVMYMoqc8mcFEaMsq5g4cpq5wx6URsJsl0dWAQ/viewform?usp=sf_link"
-    msg.attach(MIMEText(body, 'plain'))
-    text = msg.as_string()
-    server.sendmail(fromaddr, toaddr, text)
+    # toaddr = "iivanov67@yahoo.com"
+    # msg = MIMEMultipart()
+    # msg['From'] = fromaddr
+    # msg['To'] = toaddr
+    # msg['Subject'] = "Survey"
+    # body = "Take the survey:\n\n https://docs.google.com/forms/d/e/1FAIpQLSf5YSiVYvwLdVMYMoqc8mcFEaMsq5g4cpq5wx6URsJsl0dWAQ/viewform?usp=sf_link"
+    # msg.attach(MIMEText(body, 'plain'))
+    # text = msg.as_string()
+    # server.sendmail(fromaddr, toaddr, text)
 
 server.quit()
+file_to_write.close()
